a6eab56fa42c7ea64acbd162b4885838153b4f31
Julien S <durieljulien@gmail.com>
2025-08-29 21:10:05 +0000

    clean : Removing old dockerfile config

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -4,9 +4,6 @@
 dotnet restore src/cache-refresh-func/CacheRefresh.Func.csproj
 dotnet restore src/history-func/History.Api.csproj
 
-# Make hook script executable
-chmod +x infra/hook.sh
-
 # Prepare catalog-api assets
 cd src/catalog-api 
 dotnet restore && dotnet dev-certs https --trust
